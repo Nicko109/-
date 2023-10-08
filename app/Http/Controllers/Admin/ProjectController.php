6e2679c3b6c60bc4142fc653f52d6ed14c888f8a
nkara <nkaragodov@inbox.ru>
2023-10-08 22:53:01 +0300

    5)Подгрузка фронта админ панели 6)Реализовано CRUD для проектов в админке

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Project\StoreRequest;
+use App\Http\Requests\Admin\Project\UpdateRequest;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('admin.project.index', compact('projects'));
+    }
+
+    public function show(Project $project)
+    {
+        return view('admin.project.show', compact('project'));
+    }
+
+    public function create()
+    {
+        return view('admin.project.create');
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+        Project::firstOrcreate($data);
+
+        return redirect()->route('admin.project.index');
+    }
+
+    public function edit(Project $project)
+    {
+        return view('admin.project.edit', compact('project'));
+    }
+
+    public function update(Project $project, UpdateRequest $request)
+    {
+        $data = $request->validated();
+        $project->update($data);
+
+        return redirect()->route('admin.project.show', compact('project'));
+    }
+
+    public function delete(Project $project)
+    {
+
+        $project->delete();
+
+        return redirect()->route('admin.project.index');
+    }
+
+}
+
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\Guest\IndexController::class, 'index']);
+Route::get('/', [App\Http\Controllers\Main\IndexController::class, 'index']);
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', [App\Http\Controllers\Admin\MainController::class, 'index'])->name('admin.main.index');
+
+    Route::prefix('projects')->group(function () {
+        Route::get('/', [App\Http\Controllers\Admin\ProjectController::class, 'index'])->name('admin.project.index');
+        Route::get('/create', [App\Http\Controllers\Admin\ProjectController::class, 'create'])->name('admin.project.create');
+        Route::post('/', [App\Http\Controllers\Admin\ProjectController::class, 'store'])->name('admin.project.store');
+        Route::get('/{project}', [App\Http\Controllers\Admin\ProjectController::class, 'show'])->name('admin.project.show');
+        Route::get('/{project}/edit', [App\Http\Controllers\Admin\ProjectController::class, 'edit'])->name('admin.project.edit');
+        Route::patch('/{project}', [App\Http\Controllers\Admin\ProjectController::class, 'update'])->name('admin.project.update');
+        Route::delete('/{project}', [App\Http\Controllers\Admin\ProjectController::class, 'delete'])->name('admin.project.delete');
+
+});
+});
